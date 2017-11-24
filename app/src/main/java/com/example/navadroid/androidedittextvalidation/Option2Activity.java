87e4444c6a7b844fb1e390aea6489d722a477198
JanistaSrilerjan <janista_sr@cmu.ac.th>
2017-11-25 00:08:58 +0700

    add regex

--- app/src/main/java/com/example/navadroid/androidedittextvalidation/Option2Activity.java
+++ app/src/main/java/com/example/navadroid/androidedittextvalidation/Option2Activity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Option2Activity extends AppCompatActivity implements TextWatcher, View.OnClickListener{
 
     private EditText etName;
@@ -64,9 +67,65 @@ public class Option2Activity extends AppCompatActivity implements TextWatcher, V
             }
         }
     }
-
-    // To validate all EditTexts
     private boolean validateEditText(){
+        boolean isValidated = true;
+        if(!isValidName(etName.getText().toString())){
+            etName.setError("ระบุเฉพาะตัวอักษรเท่านั้น!");
+            isValidated = false;
+        }
+        if (etName.getText().toString().length() == 0) {
+            etName.setError("กรุณาระบุชื่อ");
+            isValidated = false;
+        }
+        if(!isValidEmail(etEmail.getText().toString())){
+            etEmail.setError("ระบุไม่ตรงรูปแบบ!");
+            isValidated = false;
+        }
+        if (etEmail.getText().toString().length() == 0) {
+            etEmail.setError("กรุณาระบุอีเมล์");
+            isValidated = false;
+        }
+        if (etPwd.getText().toString().length() == 0) {
+            etPwd.setError("กรุณาระบุรหัสผ่าน");
+            isValidated = false;
+        }
+        if(!isValidPhone(etPhone.getText().toString())){
+            etPhone.setError("ระบุหมายเลขโทรศัพท์ไม่ถูกต้อง!");
+            isValidated = false;
+        }
+        if (etPhone.getText().toString().length() == 0) {
+            etPhone.setError("กรุณาระบุเบอร์โทรศัพท์");
+            isValidated = false;
+        }
+        if (etPhone.getText().toString().length() < 10 || etPhone.getText().toString().length() > 10) {
+            etPhone.setError("กรุณาระบุเบอร์โทรศัพท์ 10 หลัก");
+            isValidated = false;
+        }
+        // TODO: add your EditText validation here
+
+        return isValidated;
+    }
+    private boolean isValidName(String name) {
+        String NAME_PATERN="[a-zA-Z]+\\.?";
+        Pattern pattern = Pattern.compile(NAME_PATERN);
+        Matcher matcher=pattern.matcher(name);
+        return matcher.matches();
+    }
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    private boolean isValidPhone(String phone) {
+        String PHONE_PATERN="[0-9]+";
+        Pattern pattern = Pattern.compile(PHONE_PATERN);
+        Matcher matcher=pattern.matcher(phone);
+        return matcher.matches();
+    }
+    // To validate all EditTexts
+    /*private boolean validateEditText(){
         boolean isValidated = true;
         if (etName.getText().toString().length() == 0) {
             etName.setError("Required");
@@ -75,5 +134,5 @@ public class Option2Activity extends AppCompatActivity implements TextWatcher, V
         // TODO: add your EditText validation here
 
         return isValidated;
-    }
+    }*/
 }
